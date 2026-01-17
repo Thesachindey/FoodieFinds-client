938cdf380bed44829d058efeec7e539925eb2883
Sachin <sachinscode24@gmail.com>
2026-01-18 01:50:58 +0600

    added _id

--- src/components/PopularDishes.jsx
+++ src/components/PopularDishes.jsx
@@ -82,7 +82,7 @@ const PopularDishes = async () => {
                                             ${dish.price}
                                         </div>
                                         {/* Link to the specific dynamic details page */}
-                                        <Link href={`/menu/${dish.id}`} className="btn btn-circle btn-ghost text-orange-600 hover:bg-orange-50">
+                                        <Link href={`/menu/${dish._id}`} className="btn btn-circle btn-ghost text-orange-600 hover:bg-orange-50">
                                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3" /></svg>
                                         </Link>
                                     </div>
